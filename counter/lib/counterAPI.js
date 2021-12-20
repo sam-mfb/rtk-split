4ebf962e015151aa7133e57d72835b45ba1cd7fe
Ethan Lemke <ethan@mfbtech.com>
2021-12-20 10:15:37 -0600

    remove lib

--- counter/lib/counterAPI.js
+++
@@ -1,7 +0,0 @@
-// A mock function to mimic making an async request for data
-export function fetchCount(amount) {
-    if (amount === void 0) { amount = 1; }
-    return new Promise(function (resolve) {
-        return setTimeout(function () { return resolve({ data: amount }); }, 500);
-    });
-}
--- counter/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Counter } from "./Counter";
-import { counterSliceReducer, initializeSlicePackage } from "./counterSlice";
-export { Counter, counterSliceReducer, initializeSlicePackage };
